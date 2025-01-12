2e609051739b431728e29dc34a1f4a31c40b845b
blankbutnot <plasticsbox53@gmail.com>
2025-01-12 16:23:54 +1300

    added resin recipe

--- src/main/java/com/hbm/inventory/recipes/ChemplantRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/ChemplantRecipes.java
@@ -27,7 +27,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class ChemplantRecipes extends SerializableRecipe {
-	
+
 	/**
 	 * Nice order: The order in which the ChemRecipe are added to the recipes list
 	 * Meta order: Fixed using the id param, saved in indexMapping
@@ -35,10 +35,10 @@ public class ChemplantRecipes extends SerializableRecipe {
 
 	public static HashMap<Integer, ChemRecipe> indexMapping = new HashMap<>();
 	public static List<ChemRecipe> recipes = new ArrayList<>();
-	
+
 	@Override
 	public void registerDefaults() {
-		
+
 		//6-30, formerly oil cracking, coal liquefaction and solidifciation
 		registerOtherOil();
 
@@ -70,7 +70,7 @@ public class ChemplantRecipes extends SerializableRecipe {
 			        new OreDictStack(ASBESTOS.ingot(), 1))
 		        .inputFluids(new FluidStack(Fluids.HYDROGEN, 4000))
 		        .outputFluids(new FluidStack(Fluids.THORIUM_BROMIDE, 4000)));
-		
+
 		recipes.add(new ChemRecipe(36, "COOLANT", 50)
 				.inputItems(new OreDictStack(KNO.dust()))
 				.inputFluids(new FluidStack(Fluids.WATER, 1800))
@@ -127,7 +127,7 @@ public class ChemplantRecipes extends SerializableRecipe {
 						new FluidStack(Fluids.XYLENE, 500),
 						new FluidStack(Fluids.OXYGEN, 100))
 				.outputItems(new ItemStack(ModItems.ingot_pet)));*/
-		
+
 		//Laminate Glass going here
 		recipes.add(new ChemRecipe(97, "LAMINATE", 100)
 				.inputFluids(
@@ -487,21 +487,21 @@ public class ChemplantRecipes extends SerializableRecipe {
 				.outputItems(
 						new ItemStack(ModItems.nugget_u233, 1),
 						new ItemStack(ModItems.nuclear_waste_tiny, 1)));
-		
+
 		recipes.add(new ChemRecipe(120, "MASS_CAKE", 30)
 				.inputFluids(new FluidStack(Fluids.CMILK, 4000), new FluidStack(Fluids.CREAM, 1000)) // why not regular milk? well its because the refined products allow for higher mass cakes while still needing less milk
 				.inputItems(
 						new ComparableStack(Items.sugar, 8),				// if there is a hole in my logic i will shoot myself
-						new ComparableStack(Items.egg, 4))				//ex: since a cake needs 3 buckets of milk, c-milk is more dense, leading to it being only 4 buckets of condensed milk, thats 1 bucket per cake. 
+						new ComparableStack(Items.egg, 4))				//ex: since a cake needs 3 buckets of milk, c-milk is more dense, leading to it being only 4 buckets of condensed milk, thats 1 bucket per cake.
 				.outputItems(
 						new ItemStack(Items.cake, 4)));
-		
+
 		recipes.add(new ChemRecipe(121, "BUTTER", 50)
-				.inputFluids(new FluidStack(Fluids.EMILK, 1000)) 
+				.inputFluids(new FluidStack(Fluids.EMILK, 1000))
 				.outputItems(
 						new ItemStack(ModItems.butter)));
 		recipes.add(new ChemRecipe(122, "STRAWICE", 50)
-				.inputFluids(new FluidStack(Fluids.CREAM, 1000)) 
+				.inputFluids(new FluidStack(Fluids.CREAM, 1000))
 				.inputItems(
 						new ComparableStack(ModItems.butter, 2),
 						new ComparableStack(Blocks.packed_ice, 1),
@@ -519,6 +519,11 @@ public class ChemplantRecipes extends SerializableRecipe {
 		recipes.add(new ChemRecipe(1002, "CHLOROMETHANE", 50)
 				.inputFluids(new FluidStack(Fluids.GAS, 750), new FluidStack(Fluids.CHLORINE, 250))
 				.outputFluids(new FluidStack(Fluids.CHLOROMETHANE, 1000)));
+
+		recipes.add(new ChemRecipe(1003, "RESINPRODUCTION", 200)
+			.inputs(
+				new ComparableStack(Blocks.log, 1, 3))
+			.outputFluids(new FluidStack(Fluids.RESIN, 100)));
 	}
 
 	public static void registerOtherOil() {
@@ -540,7 +545,7 @@ public class ChemplantRecipes extends SerializableRecipe {
 				.inputFluids(new FluidStack(1000, Fluids.BITUMEN))
 				.outputItems(new ItemStack(ModBlocks.asphalt, 16)));
 	}
-	
+
 	public static class ChemRecipe {
 
 		public int listing;
@@ -552,18 +557,18 @@ public class ChemplantRecipes extends SerializableRecipe {
 		public FluidStack[] outputFluids;
 		private int duration;
 		public int oxygenConsumption = 0; // How much oxygen the recipe consumes from the atmosphere per tick while processing
-		
+
 		public ChemRecipe(int index, String name, int duration) {
 			this.id = index;
 			this.name = name;
 			this.duration = duration;
 			this.listing = recipes.size();
-			
+
 			this.inputs = new AStack[4];
 			this.outputs = new ItemStack[4];
 			this.inputFluids = new FluidStack[2];
 			this.outputFluids = new FluidStack[2];
-			
+
 			if(!indexMapping.containsKey(id)) {
 				indexMapping.put(id, this);
 			} else {
@@ -575,31 +580,31 @@ public class ChemplantRecipes extends SerializableRecipe {
 			this(index, name, duration);
 			this.oxygenConsumption = oxygenConsumption;
 		}
-		
+
 		public ChemRecipe inputItems(AStack... in) {
 			for(int i = 0; i < in.length; i++) this.inputs[i] = in[i];
 			return this;
 		}
-		
+
 		public ChemRecipe inputFluids(FluidStack... in) {
 			for(int i = 0; i < in.length; i++) this.inputFluids[i] = in[i];
 			return this;
 		}
-		
+
 		public ChemRecipe outputItems(ItemStack... out) {
 			for(int i = 0; i < out.length; i++) this.outputs[i] = out[i];
 			return this;
 		}
-		
+
 		public ChemRecipe outputFluids(FluidStack... out) {
 			for(int i = 0; i < out.length; i++) this.outputFluids[i] = out[i];
 			return this;
 		}
-		
+
 		public int getId() {
 			return this.id;
 		}
-		
+
 		public int getDuration() {
 			return this.duration;
 		}
@@ -621,7 +626,7 @@ public class ChemplantRecipes extends SerializableRecipe {
 		int id = obj.get("id").getAsInt();
 		String name = obj.get("name").getAsString();
 		int duration = obj.get("duration").getAsInt();
-		
+
 		recipes.add(new ChemRecipe(id, name, duration)
 				.inputFluids(	readFluidArray(		(JsonArray) obj.get("fluidInput")))
 				.inputItems(	readAStackArray(		(JsonArray) obj.get("itemInput")))
@@ -657,7 +662,7 @@ public class ChemplantRecipes extends SerializableRecipe {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public String getComment() {
 		return "Rules: All in- and output arrays need to be present, even if empty. IDs need to be unique, but not sequential. It's safe if you add your own"
 				+ " recipes starting with ID 1000. Template order depends on the order of the recipes in this JSON file. The 'name' field is responsible for"
