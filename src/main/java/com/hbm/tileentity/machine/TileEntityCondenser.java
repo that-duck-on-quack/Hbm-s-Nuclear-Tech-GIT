7e9c2ce3a524c4e2637eaf570398cdfe85abd2c3
Cherlyte <dogchase4@gmail.com>
2025-04-15 22:29:39 -0300

    fix condenser very sorry

--- src/main/java/com/hbm/tileentity/machine/TileEntityCondenser.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityCondenser.java
@@ -122,10 +122,15 @@ public class TileEntityCondenser extends TileEntityLoadedBase implements IFluidS
 
 	public void packExtra(NBTTagCompound data) { }
 	public boolean extraCondition(int convert) {
-		if(heatExchanging && !(tanks[2].getFill() > convert/2)){
+		if(heatExchanging && tanks[2].getFill() > convert/2){
+			return true;
+		}
+		else if(!heatExchanging) {
+			return true;
+		}else{
 			return false;
 		}
-		return true;
+
 	}
 	public void postConvert(int convert) { }
 
