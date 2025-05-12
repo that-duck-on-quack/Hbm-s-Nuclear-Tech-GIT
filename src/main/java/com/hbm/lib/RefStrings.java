9ca2b9e62f04af5bcd03c6b0e949badcc92440c7
Cherlyte <dogchase4@gmail.com>
2025-05-12 00:14:27 -0300

    vbump

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -3,7 +3,7 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "NTM: Speck";
-	public static final String VERSION = "1.0.27H1.79.99 BETA (5320)";
+	public static final String VERSION = "1.0.27H1.8U BETA (5320)";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
