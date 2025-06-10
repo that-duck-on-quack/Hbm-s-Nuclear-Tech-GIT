f8c9ff4d4d14651410ca9c3161e7cb09bc8776c8
Cherlyte <dogchase4@gmail.com>
2025-06-10 13:05:47 -0300

    not the qucickest merge in the wild west

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -3,7 +3,7 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "NTM: Speck";
-	public static final String VERSION = "1.0.27H1.9 BETA (1226)";
+	public static final String VERSION = "1.0.27H100 BETA (1268)";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
