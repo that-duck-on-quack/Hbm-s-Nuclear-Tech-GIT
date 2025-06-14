594c9adf1720be09df2285d5e28fd16241962e66
Cherlyte <dogchase4@gmail.com>
2025-06-14 17:56:35 -0300

    5 millisecond fix tutorial

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineLaserBoi.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineLaserBoi.java
@@ -155,7 +155,7 @@ public class TileEntityMachineLaserBoi extends TileEntityMachineBase implements 
 			process = 0;
 
 			if (slots[1] == null && itemList.containsKey(slots[0].getItem())) {
-				slots[1] = itemList.get(slots[0].getItem());
+				slots[1] = itemList.get(slots[0].getItem()).copy();
 			} else if (slots[1] != null && slots[1].getItem() == itemList.get(slots[0].getItem()).getItem()) {
 				slots[1].stackSize++;
 			}
